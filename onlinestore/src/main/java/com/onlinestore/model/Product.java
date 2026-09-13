62080e03b828a67fb2817e0727f8bc926c959960
stelian-mihalceanu <49749036+stelian-mihalceanu@users.noreply.github.com>
2026-09-13 13:47:24 +0300

    Validate product catalog fields

--- onlinestore/src/main/java/com/onlinestore/model/Product.java
+++ onlinestore/src/main/java/com/onlinestore/model/Product.java
@@ -1,6 +1,9 @@
 package com.onlinestore.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 
 @Entity
@@ -14,8 +17,19 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Column(nullable = false)
     private String name;
+
+    @NotBlank
+    @Column(nullable = false, length = 2000)
     private String description;
+
+    @DecimalMin(value = "0.0", inclusive = true)
+    @Column(nullable = false)
     private double price;
+
+    @PositiveOrZero
+    @Column(nullable = false)
     private int stock;
 }
