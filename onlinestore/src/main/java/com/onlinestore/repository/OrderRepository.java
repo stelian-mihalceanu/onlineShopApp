76267d62a4d0fb4a6314b9725dbc8ff3dc93a21c
stelian-mihalceanu <49749036+stelian-mihalceanu@users.noreply.github.com>
2026-09-13 13:45:56 +0300

    Add order repository

---
+++ onlinestore/src/main/java/com/onlinestore/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.onlinestore.repository;
+
+import com.onlinestore.model.Order;
+import com.onlinestore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserOrderByCreatedAtDesc(User user);
+}
